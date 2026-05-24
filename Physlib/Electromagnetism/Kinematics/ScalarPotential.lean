f2cc2764e1d6629003438f3ea939c6d94de379f5
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2026-05-24 07:02:30 +0100

    refactor: Move distributional EM to new file system (#1118)
    * move: DistElectromagneticPotential
    
    * rm: DistElectromagneticPotential
    
    * move: scalarPotential
    
    * rm: scalarPotential
    
    * move: vectorPotential
    
    * rm: vectorPotential
    
    * move: FieldStrength
    
    * rm: FieldStrength
    
    * move: ElectricField
    
    * rm: ElectricField
    
    * move: MagneticFields
    
    * rm: MagneticFields
    
    * move: KineticTerm
    
    * rm: KineticTerm
    
    * move: CurrentDensity
    
    * rm: CurrentDensity
    
    * move: Lagrangian
    
    * rm: Lagrangian
    
    * move: IsExtrema
    
    * rm: IsExtrema
    
    * fix: Examples
    
    * fix: imports
    
    * docs: rm old docs
    
    * docs: fix new docs

--- Physlib/Electromagnetism/Kinematics/ScalarPotential.lean
+++ Physlib/Electromagnetism/Kinematics/ScalarPotential.lean
@@ -27,8 +27,6 @@ the scalar potential is non-relativistic and is therefore a function of `Time` a
 
 - `ElectromagneticPotential.scalarPotential` : The scalar potential from an
   electromagnetic potential.
-- `DistElectromagneticPotential.scalarPotential` : The scalar potential from an
-  electromagnetic potential which is a distribution.
 
 ## iii. Table of contents
 
@@ -36,7 +34,6 @@ the scalar potential is non-relativistic and is therefore a function of `Time` a
 - B. Relation to constructors
 - C. Smoothness of the Scalar Potential
 - D. Differentiability of the Scalar Potential
-- E. Scalar potential for distributions
 
 ## iv. References
 
@@ -201,36 +198,4 @@ lemma scalarPotential_differentiable_time {d} (c : SpeedOfLight) (A : Electromag
 
 end ElectromagneticPotential
 
-/-!
-
-## E. Scalar potential for distributions
-
--/
-
-namespace DistElectromagneticPotential
-open TensorSpecies
-open Tensor
-open SpaceTime
-open TensorProduct
-open minkowskiMatrix
-attribute [-simp] Fintype.sum_sum_type
-attribute [-simp] Nat.succ_eq_add_one
-
-set_option backward.isDefEq.respectTransparency false in
-/-- The scalar potential of an electromagnetic potential which is a distribution. -/
-noncomputable def scalarPotential {d} (c : SpeedOfLight) :
-    DistElectromagneticPotential d →ₗ[ℝ]
-    (Time × Space d) →d[ℝ] ℝ where
-  toFun A := Lorentz.Vector.temporalCLM d ∘L distTimeSlice c (c.val • A)
-  map_add' A₁ A₂ := by
-    ext ε
-    simp [distTimeSlice]
-  map_smul' r A := by
-    ext ε
-    simp only [distTimeSlice, map_smul, ContinuousLinearEquiv.coe_mk, LinearEquiv.coe_mk,
-      LinearMap.coe_mk, AddHom.coe_mk, ContinuousLinearMap.coe_comp', ContinuousLinearMap.coe_smul',
-      Function.comp_apply, Pi.smul_apply, smul_eq_mul, Real.ringHom_apply]
-    ring
-
-end DistElectromagneticPotential
 end Electromagnetism
